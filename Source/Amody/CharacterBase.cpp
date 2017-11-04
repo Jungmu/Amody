615b35f35813c07747636785b7f7077e032b0e88
jungmu <whdrjs0@gmail.com>
2017-11-05 02:50:39 +0900

    조이스틱으로 캐릭터 움직이는 것 구현

---
+++ Source/Amody/CharacterBase.cpp
@@ -0,0 +1,48 @@
+// Copyright JJ 2017-
+
+#include "CharacterBase.h"
+#include "Classes/Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ACharacterBase::ACharacterBase()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACharacterBase::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	float moveY = InputComponent->GetAxisValue("MoveY");
+	float moveX = InputComponent->GetAxisValue("MoveX");
+
+	FRotator rotator = UKismetMathLibrary::MakeRotFromX(FVector(moveX, moveY, 0.f));
+
+	SetActorRotation(rotator);
+}
+
+// Called to bind functionality to input
+void ACharacterBase::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+	InputComponent->BindAxis("MoveY", this, &ACharacterBase::MoveForward);
+	InputComponent->BindAxis("MoveX", this, &ACharacterBase::MoveForward);
+}
+
+void ACharacterBase::MoveForward(float amount) 
+{
+	if (Controller && amount)
+	{		
+		AddMovementInput(GetActorForwardVector(),FMath::Abs(amount)*MoveSpeed*FApp::GetDeltaTime());
+	}
+	
+}
---
+++ Source/Amody/CharacterBase.h
@@ -0,0 +1,33 @@
+// Copyright JJ 2017-
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "CharacterBase.generated.h"
+
+UCLASS()
+class AMODY_API ACharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACharacterBase();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	void MoveForward(float amount);
+
+	UPROPERTY(EditAnywhere)
+		float MoveSpeed = 100;
+};
