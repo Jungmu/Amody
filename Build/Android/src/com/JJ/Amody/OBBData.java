2761b1da621d7a47cf9d8808c872e78634bf05fa
jungmu <whdrjs0@gmail.com>
2017-11-12 01:52:16 +0900

    인벤토리 시스템 구현.

--- Build/Android/src/com/JJ/Amody/OBBData.java
+++ Build/Android/src/com/JJ/Amody/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-64908298L // the length of the file in bytes
+73771540L // the length of the file in bytes
 )
 };
 };
