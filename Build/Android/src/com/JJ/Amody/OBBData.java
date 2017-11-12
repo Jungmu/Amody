8d8ae2a8c44692f831d84a418e583fe16f6b5fa4
jungmu <whdrjs0@gmail.com>
2017-11-12 12:13:15 +0900

    라이팅 버그 픽스

--- Build/Android/src/com/JJ/Amody/OBBData.java
+++ Build/Android/src/com/JJ/Amody/OBBData.java
@@ -17,7 +17,7 @@ public static final XAPKFile[] xAPKS = {
 new XAPKFile(
 true, // true signifies a main file
 "1", // the version of the APK that the file was uploaded against
-73771540L // the length of the file in bytes
+73942423L // the length of the file in bytes
 )
 };
 };
